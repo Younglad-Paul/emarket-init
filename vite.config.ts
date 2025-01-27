10d69c457c3d6097f9af9bb7bee77b792d050936
Younglad-Paul <borisadedayopaul@gmail.com>
2025-01-27 10:34:46 +0100

    fixed router

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
-    open: true,
+    host: true,
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
+  preview: {
+    port: 5000,
   },
-})
+});
